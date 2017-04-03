8af7da58c32688284b82a86633c1bf39322aeffa
domd0101 <ddecicco@buffalo.edu>
2017-04-03 14:04:21 -0600

    getting there

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,24 +1,68 @@
 angular.module('app1')
   .controller('mainCtrl',['$scope','$http',function($scope,$http){
-    $scope.test='test';
     $scope.map;
 
-
-    $scope.gMaps = (function(){
-      (function myMap() {
-          var mapProp= {
-              center:new google.maps.LatLng(40.226058, -111.662261),
-              zoom:15,
-          };
-      $scope.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-        })();
+    $scope.current = (function(){
+      var promise = $http.get('http://ip-api.com/json');
+        return promise.then(function(result){
+          $scope.num = 'one';
+          $scope.data = result.data;
+          $scope.gMaps($scope.data,$scope.num);
+        })
     })();
 
+    $scope.gMaps = function(data,para){
+      (function myMap() {
+        if(para==='one'){
+          var mapProp = {
+            center: new google.maps.LatLng($scope.data.lat, $scope.data.lon),
+            zoom: 12,
+          }
+        }
+        else if(para==='two'){
+           var mapProp = {
+              center: new google.maps.LatLng($scope.data.lat, $scope.data.lng),
+              zoom:12
+          }
+        }
+        $scope.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+          })();
+        }
+            ///++++Render Original Map+++++////
+
+      //   var request = {
+      //        types: ['restaurant']
+      //      };
+      //  infowindow = new google.maps.InfoWindow();
+      //      places = new google.maps.places.PlacesService($scope.map);
+      //     places.nearbySearch(request,);
+          // centerMarker = new google.maps.Marker({
+          //     position: center,
+          //     animation: google.maps.Animation.DROP,
+          //     map: map
+          //   });
+          // }
+
+
+
+    $scope.newloc = function(zip){
+      var zip = zip;
+      var promise = $http.get("http://ziplocate.us/api/v1/"+zip+"");
+        return promise.then(function(result){
+          console.log('working');
+          $scope.data = result.data;
+          $scope.num = 'two';
+          $scope.gMaps($scope.data,$scope.num);
+        })
+    }
+
+    // $scope.food = function(some){
+    //   var somefood = some;
+    //
+    // }
+
+
+
+    ///&radius=500&type=restaurant&keyword=cruise&key=
+
   }])
-  // var promise = $http.get('https://maps.googleapis.com/maps/api/js?key=AIzaSyB2f09BzLRIDauS6VVvHpyMuzn1ZfVRocc&callback=myMap')
-  //   return promise.then(function(results){
-  //     $scope.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  //     console.log('promise then');
-  //   })
-  // <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2f09BzLRIDauS6VVvHpyMuzn1ZfVRocc&callback=myMap"
-  //   type="text/javascript"></script>
